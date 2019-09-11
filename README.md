215464edfd9995a366336a21ae1091c47d5c7327
Paul Logston <paul.logston@gmail.com>
2019-09-10 23:25:35 -0400

    Add validators to latitude and longitude

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ docker build -t gcr.io/unified-academy-252221/liberia .
 docker push gcr.io/unified-academy-252221/liberia
 kubectl set image deployment/liberia-app liberia=gcr.io/unified-academy-252221/liberia:latest 
 
+kubectl exec -it liberia-deployment-68cbf778f-46h6h /bin/sh
+kubectl apply -f deployment.yml
+
 kubectl create secret generic cloudsql-db-credentials --from-env-file=./secrets/secrets.env
 kubectl create secret generic cloudsql-instance-credentials --from-file=credentials.json=./secrets/unified-academy-252221-b53c35df2dad.json
 
--- shops/models.py
+++ shops/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -9,11 +10,19 @@ class Shop(models.Model):
     latitude = models.DecimalField(
         max_digits=17,
         decimal_places=15,
+        validators=[
+            MaxValueValidator(-7.289515),
+            MinValueValidator(-11.640186)
+        ],
     )
 
     longitude = models.DecimalField(
         max_digits=17,
         decimal_places=15,
+        validators=[
+            MaxValueValidator(8.575844),
+            MinValueValidator(4.017699)
+        ],
     )
 
     items = models.ManyToManyField('shops.Item')
